93dc92b5d4a39cdc0cd3dd27fb75a3d4398d5e0b
Leinner-Lopez <leinner1006@gmail.com>
2026-04-07 02:22:08 -0500

    feat: Añadir login con validaciones del formulario y enrutamiento.
    Introduce un formulario de inicio de sesión con validación de correo electrónico y contraseña, manejo de errores y estilos de formulario. Define rutas de navegación para inicio de sesión y registro, y añade variables de tema personalizado para una interfaz de usuario consistente. Mejora el flujo de autenticación e incorporación de usuarios.

---
+++ src/app/Features/Auth/login/login.ts
@@ -0,0 +1,63 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../Core/Services/auth-service';
+import { Router } from '@angular/router';
+import { LoginRequest } from '../../../Data/Interfaces/LoginRequest';
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterLink,ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  form = inject(FormBuilder);
+  authService = inject(AuthService);
+  router = inject(Router);
+
+
+  formularioLogin: FormGroup = this.form.group({
+    Username: ['', [Validators.required, Validators.email]],
+    Password: ['', Validators.required]
+  })
+
+  onSubmit() {
+    if(this.formularioLogin.invalid) return;
+    const credentials: LoginRequest= {
+      username: this.formularioLogin.value.Username,
+      password: this.formularioLogin.value.Password
+    }
+    this.authService.login(credentials).subscribe({
+      next: () => {
+        const role = this.authService.getUserRole();
+        console.log('User role:', role);
+        console.log('Email:', this.authService.getUserEmail());
+      },error: (err) => {
+        console.error('Login failed', err);
+      }
+    })
+  }
+
+  private redirectByUserRole(role: string | null) {
+        switch (role) {
+      case 'ADMIN':
+        this.router.navigate(['/admin/inicio']);
+        break;
+      case 'CUSTOMER':
+        this.router.navigate(['/customer/inicio']);
+        break;
+      case 'STORE':
+        this.router.navigate(['/store/inicio']);
+        break;
+      default:
+        this.router.navigate(['/login']);
+        break;
+    }
+  }
+
+  hasError(controlName: string, errorType: string): boolean {
+    const control = this.formularioLogin.get(controlName);
+    return control?.hasError(errorType) && (control.dirty || control.touched) || false;
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './Features/Auth/login/login';
+import { Register } from './Features/Auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+];
