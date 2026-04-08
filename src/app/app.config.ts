0b368c34525cec93ca80571d869c5b09aa52f417
Leinner-Lopez <leinner1006@gmail.com>
2026-04-07 21:23:32 -0500

    feat: Añadir autenticación y guards basados en roles con interceptors
    Introduce autenticación y protección de rutas basada en roles para proteger las rutas según el estado y los roles del usuario. Añade un interceptor HTTP para la inyección automática de tokens en las solicitudes de API. Integra el interceptor en la configuración de la aplicación para aplicar la autenticación en la capa de red.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './Core/Interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withFetch()),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
