f108cd88a19fa0538aa47c169a2a628f106167c0
Leinner-Lopez <leinner1006@gmail.com>
2026-04-09 02:39:47 -0500

    feat:Agrega un modal para la creación y edición de categorías en la interfaz de administración.
    Introduce un cuadro de diálogo modal reutilizable para gestionar el registro y la edición de categorías.
    
    Actualiza las dependencias para incluir el CDK de Angular necesario y el estilo para la compatibilidad con el modal.

--- src/app/Features/Admin/categories/categories.ts
+++ src/app/Features/Admin/categories/categories.ts
@@ -1,6 +1,8 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { CategoryService } from '../../../Data/Services/category-service';
 import { Category } from '../../../Data/Interfaces/Category';
+import { RegisterCategory } from "../../../Shared/Modals/register-category/register-category";
+import { Dialog } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-categories',
@@ -9,16 +11,50 @@ import { Category } from '../../../Data/Interfaces/Category';
   styleUrl: './categories.css',
 })
 export class Categories implements OnInit {
+  dialog = inject(Dialog);
   categoryService: CategoryService = inject(CategoryService);
-  categories:Category[] = [];
+  categories = signal<Category[]>([]);
   categoriesNumber = signal(0);
 
   ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories() {
     this.categoryService.getAllCategories().subscribe((data) => {
-      this.categories = data;
+      this.categories.set(data);
       this.categoriesNumber.set(data.length);
-      console.log(this.categories);
     });
   }
 
+  registerCategory() {
+    const dialogRef = this.dialog.open<string>(RegisterCategory, {
+
+    });
+    dialogRef.closed.subscribe(result => {
+      if (result === 'Categoría registrada') {
+        this.getCategories();
+      }
+    });
+  }
+
+  deleteCategory(categoryId: number) {
+    this.categoryService.deleteCategory(categoryId).subscribe({
+      next: () => {
+        console.log('Categoría eliminada exitosamente');
+        this.getCategories();
+      }
+    })
+  }
+
+  updateCategory(categoryId: number) {
+    const dialogRef = this.dialog.open<string>(RegisterCategory, {
+      data: { categoryId }
+    });
+    dialogRef.closed.subscribe(result => {
+      if (result === 'Categoría registrada') {
+        this.getCategories();
+      }
+    });
+  }
 }
---
+++ src/app/Shared/Modals/register-category/register-category.ts
@@ -0,0 +1,67 @@
+import { Component, Inject, inject, model } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CategoryService } from '../../../Data/Services/category-service';
+import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
+import { Category } from '../../../Data/Interfaces/Category';
+
+@Component({
+  selector: 'app-register-category',
+  imports: [ReactiveFormsModule],
+  templateUrl: './register-category.html',
+  styleUrl: './register-category.css',
+})
+export class RegisterCategory {
+
+  dialogRef: DialogRef<string> = inject(DialogRef);
+  form = inject(FormBuilder);
+  categoryService: CategoryService = inject(CategoryService);
+
+  constructor(@Inject(DIALOG_DATA) public data: any) {
+    if (data && data.categoryId) {
+      this.categoryService.findCategoryById(data.categoryId).subscribe(category => {
+        this.formularioRegistration.patchValue({
+          name: category.name,
+          description: category.description
+        });
+      });
+    }
+
+  }
+
+  formularioRegistration = this.form.group({
+    name: ['', [Validators.required]],
+    description: ['', [Validators.required]]
+  })
+
+  onSubmit() {
+    if (this.formularioRegistration.invalid) return;
+
+    const categoryData: Category = {
+      category_id: null!,
+      name: this.formularioRegistration.value.name!,
+      description: this.formularioRegistration.value.description!
+    }
+
+    if(this.data && this.data.categoryId) {
+      categoryData.category_id = this.data.categoryId;
+      this.categoryService.updateCategory(categoryData).subscribe({
+        next: () => {
+          console.log('Categoría actualizada exitosamente');
+          this.closeModal();
+        }
+      });
+      return;
+    }
+    
+    this.categoryService.saveCategory(categoryData).subscribe({
+      next: () => {
+        console.log('Categoría registrada exitosamente');
+        this.closeModal();
+      }
+    });
+  }
+
+  closeModal() {
+    this.dialogRef.close('Categoría registrada');
+  }
+}
